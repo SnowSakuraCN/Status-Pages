fa61faeb218455bac1f397248ce2b32199a2b152
SnowSakura <71383712+SnowSakuraCN@users.noreply.github.com>
2022-01-29 18:49:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ window.Config = {
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
@@ -30,12 +30,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Snowsakrua 主站',
-      url: 'https://status.org.cn/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: 'Snowsakrua 博客主站',
+      url: 'https://snowsakrua.io/'
     }
   ]
 };
